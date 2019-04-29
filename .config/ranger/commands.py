8d49fa51dbf25627aa86f111751734045e577969
tdro <tdro@users.noreply.github.com>
2019-04-28 21:53:15 -0400

    .config/ranger/commands: fzf_select sort by time and use xdg-open

--- .config/ranger/commands.py
+++ .config/ranger/commands.py
@@ -23,7 +23,7 @@ class fzf_select(Command):
     def execute(self):
         import subprocess
         import os.path
-        fzf = self.fm.execute_command("ls -A1 --color=never | fzf +m", universal_newlines=True, stdout=subprocess.PIPE)
+        fzf = self.fm.execute_command("ls -t -A1 --color=never | fzf +m", universal_newlines=True, stdout=subprocess.PIPE)
         stdout, stderr = fzf.communicate()
         if fzf.returncode == 0:
             fzf_file = os.path.abspath(stdout.rstrip('\n'))
@@ -31,3 +31,4 @@ class fzf_select(Command):
                 self.fm.cd(fzf_file)
             else:
                 self.fm.select_file(fzf_file)
+                self.fm.run('xdg-open ' + fzf_file)
