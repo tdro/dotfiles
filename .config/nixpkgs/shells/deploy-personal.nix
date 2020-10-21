e033877d947a9d7364a25f247fecf74c9fe8cff1
tdro <tdro@users.noreply.github.com>
2020-10-21 03:50:43 -0400

    .config/nixpkgs/shells/deploy-personal: Add version to PS1

--- .config/nixpkgs/shells/deploy-personal.nix
+++ .config/nixpkgs/shells/deploy-personal.nix
@@ -15,6 +15,6 @@ mkShell {
     python -m pip install mitogen
     export ANSIBLE_STRATEGY_PLUGINS=$virtualenvs/ansible-mitogen/lib/python3.8/site-packages/ansible_mitogen/plugins
     export ANSIBLE_STRATEGY=mitogen_linear
-    export PS1='\h (deploy-personal) \W \$ '
+    export PS1='\h (personal ${version}) \W \$ '
   '';
 }
