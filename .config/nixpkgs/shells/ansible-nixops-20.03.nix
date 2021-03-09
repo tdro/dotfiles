5c27ee47cb7a05c0546f28b8c4c39ae02f243888
tdro <tdro@users.noreply.github.com>
2021-03-08 19:44:34 -0500

    .config/nixpkgs/shells/ansible-nixops: Pin mitogen version 0.2.9
    .config/nixpkgs/shells/ansible-nixops-20.03.nix
    .config/nixpkgs/shells/ansible-nixops-20.09.nix

--- .config/nixpkgs/shells/ansible-nixops-20.03.nix
+++ .config/nixpkgs/shells/ansible-nixops-20.03.nix
@@ -1,12 +1,11 @@
-with import <nixpkgs> {};
+with import <nixpkgs> { };
 
 let
   project = "${builtins.getEnv "HOME"}/Shares/Projects/infrastructure";
   version = (import "${project}/nixos/versions.nix")."20.03".version;
   channel = (import "${project}/nixos/versions.nix")."20.03".channel;
-in
 
-mkShell {
+in mkShell {
   name = "ansible-nixops-${version}";
   buildInputs = [ python38.pkgs.pip ];
   shellHook = ''
@@ -17,7 +16,7 @@ mkShell {
     mkdir -p $virtualenvs
     python -m venv $virtualenvs/ansible-mitogen
     . $virtualenvs/ansible-mitogen/bin/activate
-    python -m pip install mitogen
+    pip install mitogen==0.2.9
     export ANSIBLE_STRATEGY_PLUGINS=$virtualenvs/ansible-mitogen/lib/python3.8/site-packages/ansible_mitogen/plugins
     export ANSIBLE_STRATEGY=mitogen_linear
     export PS1='\h (nixos ${version} ''${_NIX_CHANNEL##*.}) \W \$ '
