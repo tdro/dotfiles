5b712fc5557c31b494b73e797d4c167495d09b7e
tdro <tdro@users.noreply.github.com>
2021-05-12 04:15:01 -0400

    .config/emacs/init: Compact and disable more options

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,34 +1,29 @@
 (custom-set-variables
  '(package-selected-packages (quote (evil)))
- '(tool-bar-mode nil)
- ;; set slime history file location
- '(slime-repl-history-file "~/.cache/slime-history.eld"))
+ '(slime-repl-history-file "~/.cache/slime-history.eld")) ; Set slime history file location.
 
 (custom-set-faces
  '(region ((t (:background "color-238")))))
 
-;; disable startup screen
-(setq inhibit-startup-screen t)
+(setq inhibit-startup-screen t) ; Disable startup screen.
+(setq-default indent-tabs-mode nil) ; Use spaces for indentation.
 
-;; disable menu bar
-(menu-bar-mode -1)
+(menu-bar-mode -1)   ; Disable menu bar.
+(tool-bar-mode -1)   ; Disable tool bar.
+(tooltip-mode -1)    ; Disable tool tips.
+(scroll-bar-mode -1) ; Disable scroll bar.
+(set-fringe-mode 10) ; Create space.
 
-;; use spaces for indentation
-(setq-default indent-tabs-mode nil)
-
-;; set up package.el to work with melpa
-(require 'package)
+(require 'package) ; Set up package.el to work with melpa.
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
-;; enable slime
-(setq inferior-lisp-program "sbcl")
+(setq inferior-lisp-program "sbcl") ; Enable slime.
 (unless (package-installed-p 'slime)
   (package-install 'slime))
 
-;; enable evil
-(unless (package-installed-p 'evil)
+(unless (package-installed-p 'evil) ; Enable evil.
   (package-install 'evil))
 (require 'evil)
 (evil-mode 1)
