9b9db1efa58f1ea0a2ad7e933c980e8c06f76b82
tdro <tdro@users.noreply.github.com>
2021-07-28 23:45:59 -0400

    .vim/snippets/module: Use more refined snippet template

--- .vim/snippets/module.nix
+++ .vim/snippets/module.nix
@@ -4,15 +4,36 @@ let
 
   service = "";
   cfg = config.services.${service};
+  settings = pkgs.formats.json { };
 
 in {
 
   options.services.${service} = {
+
     enable = lib.mkOption {
       type = lib.types.bool;
       default = false;
     };
+
+    user = lib.mkOption {
+      type = lib.types.str;
+      default = service;
+    };
+
+    group = lib.mkOption {
+      type = lib.types.str;
+      default = service;
+    };
+
+    settings = lib.mkOption {
+      type = settings.type;
+      default = { };
+    };
   };
 
-  config = lib.mkIf cfg.enable { };
+  config = lib.mkIf cfg.enable {
+
+    services.${service}.settings = { };
+
+  };
 }
