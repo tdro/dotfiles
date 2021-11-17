88acd96eb8215a2e5afb1401b4a0164371bd245e
tdro <tdro@users.noreply.github.com>
2021-11-17 17:43:21 -0500

    .config/nixpkgs/packages/ruamel.yaml.cmd: All in one default.nix

--- .config/nixpkgs/packages/ruamel.yaml.cmd/default.nix
+++ .config/nixpkgs/packages/ruamel.yaml.cmd/default.nix
@@ -1,6 +1,4 @@
-{ lib, python38 }:
-
-with python38.pkgs;
+{ lib, pkgs, python39 }:
 
 let
 
@@ -10,10 +8,10 @@ let
     homepage = "https://sourceforge.net/p/ruamel-yaml-cmd/code/ci/default/tree";
   };
 
-  ruamel.std.argparse = buildPythonPackage rec {
+  ruamel.std.argparse = python39.pkgs.buildPythonPackage rec {
     pname = "ruamel.std.argparse";
     version = "0.8.3";
-    src = fetchPypi {
+    src = python39.pkgs.fetchPypi {
       inherit pname version;
       sha256 = "0srv4g6jryyaz26csx315pbmfhm4rw8jhfsl7rq7krrglgqwjryi";
     };
@@ -21,44 +19,61 @@ let
     inherit meta;
   };
 
-  ruamel.std.convert = buildPythonPackage rec {
+  ruamel.std.convert = python39.pkgs.buildPythonPackage rec {
     pname = "ruamel.yaml.convert";
     version = "0.3.2";
-    src = fetchPypi {
+    src = python39.pkgs.fetchPypi {
       inherit pname version;
       sha256 = "1i6919b5f5ygj7y7c4x0wi91n8y0mw4nf9f0bcfjk29i594xjph6";
     };
-    propagatedBuildInputs = [ python-dateutil ruamel_yaml ];
+    propagatedBuildInputs = [ python39.pkgs.python-dateutil python39.pkgs.ruamel_yaml ];
     doCheck = false;
     inherit meta;
   };
 
-  ruamel.yaml.cmd = buildPythonPackage rec {
+  ruamel.yaml.cmd = python39.pkgs.buildPythonPackage rec {
     pname = "ruamel.yaml.cmd";
     version = "0.5.6";
-    src = fetchPypi {
+    src = python39.pkgs.fetchPypi {
       inherit pname version;
       sha256 = "0zc2h6b721r8c05dm7y8zgrgbvifbgcm8jmqvns7ar4ajll3cvwv";
     };
-    propagatedBuildInputs = [ configobj ruamel.std.argparse ruamel.std.convert ];
-    patches = [ ./disable-backup-files.patch ];
+    propagatedBuildInputs = [ python39.pkgs.configobj ruamel.std.argparse ruamel.std.convert ];
+    patches = [
+      (pkgs.writeTextFile {
+        name = "disable-backup-files.patch";
+        text = ''
+          --- a/yaml_cmd.py	2020-12-14 22:13:19.000000000 -0500
+          +++ b/yaml_cmd.py	2020-12-14 22:13:34.067115865 -0500
+          @@ -513,8 +513,6 @@
+               def round_trip_save(self, file_name):
+                   inp = open(file_name).read()
+                   backup_file_name = file_name + '.orig'
+          -        if not os.path.exists(backup_file_name):
+          -            os.rename(file_name, backup_file_name)
+                   return self.round_trip_single(inp, out_file=file_name)
+
+               def round_trip_input(self, inp):
+        '';
+      })
+    ];
     doCheck = false;
     dontWrapPythonPrograms = true;
     inherit meta;
   };
 
-in buildPythonApplication rec {
+in python39.pkgs.buildPythonApplication rec {
 
   pname = ruamel.yaml.cmd.pname;
   version = ruamel.yaml.cmd.version;
   propagatedBuildInputs = [ ruamel.yaml.cmd ];
-  pythonEnvironment = python38.withPackages (_: propagatedBuildInputs);
+  pythonEnvironment = python39.withPackages (_: propagatedBuildInputs);
 
   installPhase = ''
     runHook preInstall
     mkdir -p $out/bin
     cp ${ruamel.yaml.cmd}/bin/yaml $out/bin
-    sed -i 's|^#!.*$|#!${pythonEnvironment}/bin/python3.8|' $out/bin/yaml
+    sed -i 's|^#!.*$|#!${pythonEnvironment}/bin/python3.9|' $out/bin/yaml
     runHook postInstall
   '';
 
