d10fb65a5d3038810ac4d8bf2a2fbbd5b789c4da
tdro <tdro@users.noreply.github.com>
2022-02-19 01:20:40 -0500

    .config/nixpkgs/helpers/mkShellMinimal: Add usage

--- .config/nixpkgs/helpers/mkShellMinimal.nix
+++ .config/nixpkgs/helpers/mkShellMinimal.nix
@@ -1,5 +1,22 @@
 ### Source:  https://github.com/NixOS/nixpkgs/commit/459771518d44f60b59a19381d07b12297908215d
 ### Article: https://fzakaria.com/2021/08/02/a-minimal-nix-shell.html
+### Usage:
+
+# let
+#
+#   pkgs = import <nixpkgs> { };
+#   mkShellMinimal = pkgs.callPackage (builtins.fetchurl {
+#     url = "https://raw.githubusercontent.com/tdro/dotfiles/ae8c382ca82135ad2910cb3ef18c4e6f31ff0fde/.config/nixpkgs/helpers/mkShellMinimal.nix";
+#     sha256 = "123ax7v6fbj1cq0kvr8jiphg99k04zw6b4r5dnn94fj1gmd2hxay";
+#   }) { };
+#
+# in mkShellMinimal {
+#   name = "minimal-shell";
+#   packages = [ pkgs.hello ];
+#   shellHook = ''
+#     printf 'hello world'
+#   '';
+# }
 
 { writeTextFile, writeScript, system }:
 
