40473cd8f0e5dbe11fd7942c4adabbe9782cf044
tdro <tdro@users.noreply.github.com>
2023-03-28 05:56:02 -0400

    .config/emacs/init.el: Add backup location
    Remove evil

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -10,7 +10,6 @@
 (setq inferior-lisp-program "sbcl") ; Enable slime.
 (setq-default mode-line-format nil) ; Remove status line.
 
-
 (menu-bar-mode   -1)    ; Disable menu bar.
 (tool-bar-mode   -1)    ; Disable tool bar.
 (tooltip-mode    -1)    ; Disable tool tips.
@@ -18,14 +17,15 @@
 (set-fringe-mode 10)    ; Create space.
 (ido-mode t)            ; Enable file search interactive mode C-x C-f.
 
+(setq backup-directory-alist
+  '(("." . "~/.config/emacs/backups")))
+
 (require 'package)                                             ; Set up package.el to work with melpa.
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 (unless package-archive-contents (package-refresh-contents))
 (package-install-selected-packages)
-
-(evil-mode 1)                                                  ; Enable evil.
 
 (global-set-key (kbd "M-x") 'smex)                             ; Enable smex interactive M-x.
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
