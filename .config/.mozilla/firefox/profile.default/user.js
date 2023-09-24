1a90d9ec47ee8081c6db699f2c61c5e9111d0121
tdro <tdro@users.noreply.github.com>
2023-09-24 13:59:54 -0400

    .config/.mozilla/firefox/profile.default/user.js: Update
    Add some comments

--- .config/.mozilla/firefox/profile.default/user.js
+++ .config/.mozilla/firefox/profile.default/user.js
@@ -9,20 +9,29 @@ user_pref("app.update.auto", false);
 
 user_pref("beacon.enabled", false);
 user_pref("breakpad.reportURL", "");
-user_pref("browser.aboutConfig.showWarning", false);
 
-user_pref("browser.cache.disk.enable", false);
+user_pref("browser.aboutConfig.showWarning", false);                        // Disable about:config warning
+user_pref("browser.discovery.enabled", false);                              // Disable extension recommendations
+user_pref("browser.fixup.alternate.enabled", false);                        // Disable URL fixing
+user_pref("browser.formfill.enable", false);                                // Disable form history
+user_pref("browser.pocket.enabled", false);                                 // Disable pocket
+user_pref("browser.privatebrowsing.forceMediaMemoryCache", true);           // Memory only private browsing media cache
+user_pref("browser.startup.homepage", "about:blank");                       // Set home page to about:blank
+
+user_pref("browser.cache.disk.enable", false);                              // Disable disk cache
 user_pref("browser.cache.disk_cache_ssl", false);
 user_pref("browser.cache.memory.capacity", 25600);
 user_pref("browser.cache.offline.enable", false);
 
+user_pref("browser.download.alwaysOpenPanel", false);                       // Disable opening panel on download
+user_pref("browser.download.always_ask_before_handling_new_types", true);   // Always ask for new mime types
+user_pref("browser.download.animateNotifications", false);                  // Disable download animations
+user_pref("browser.download.manager.addToRecentDocs", false);               // Disable adding download to recent list
+user_pref("browser.download.useDownloadDir", false);                        // Always ask where to save download
+
 user_pref("browser.crashReports.unsubmittedCheck.autoSubmit", false);
 user_pref("browser.crashReports.unsubmittedCheck.autoSubmit2", false);
 user_pref("browser.crashReports.unsubmittedCheck.enabled", false);
-
-user_pref("browser.discovery.enabled", false);
-user_pref("browser.download.animateNotifications", false);
-user_pref("browser.fixup.alternate.enabled", false);
 
 user_pref("browser.newtab.preload", false);
 user_pref("browser.newtabpage.enabled", false);
@@ -40,9 +49,6 @@ user_pref("browser.newtabpage.activity-stream.section.highlights.includePocket",
 user_pref("browser.newtabpage.activity-stream.section.highlights.includeVisited", false);
 user_pref("browser.newtabpage.activity-stream.section.highlights.includeBookmarks", false);
 user_pref("browser.newtabpage.activity-stream.section.highlights.includeDownloads", false);
-
-user_pref("browser.pocket.enabled", false);
-user_pref("browser.startup.homepage", "about:blank");
 
 user_pref("browser.safebrowsing.appRepURL", "");
 user_pref("browser.safebrowsing.blockedURIs.enabled", false);
@@ -65,13 +71,15 @@ user_pref("browser.selfsupport.url", "");
 user_pref("browser.disableResetPrompt", true);
 user_pref("browser.shell.checkDefaultBrowser", false);
 user_pref("browser.send_pings.require_same_host", true);
-user_pref("browser.startup.homepage_override.mstone", "ignore");
+user_pref("browser.startup.homepage_override.mstone", "ignore");        // Disable welcome screen
+user_pref("browser.messaging-system.whatsNewPanel.enabled", false);     // Disable what's new panel
 user_pref("browser.sessionstore.privacy_level", 2);
 user_pref("browser.sessionhistory.max_entries", 20);
 user_pref("browser.tabs.crashReporting.sendReport", false);
 
 user_pref("browser.urlbar.maxRichResults", 0);
 user_pref("browser.urlbar.searchSuggestionsChoice", false);
+user_pref("browser.urlbar.showSearchTerms.enabled", false);             // Always show URL
 user_pref("browser.urlbar.speculativeConnect.enabled", false);
 user_pref("browser.urlbar.suggest.bookmark", false);
 user_pref("browser.urlbar.suggest.engines", false);
@@ -79,6 +87,7 @@ user_pref("browser.urlbar.suggest.history", false);
 user_pref("browser.urlbar.suggest.openpage", false);
 user_pref("browser.urlbar.suggest.searches", false);
 user_pref("browser.urlbar.suggest.topsites", false);
+user_pref("browser.urlbar.showSearchSuggestionsFirst", false);
 user_pref("browser.urlbar.timesBeforeHidingSuggestionsHint", 0);
 user_pref("browser.urlbar.trimURLs", false);
 
@@ -109,7 +118,8 @@ user_pref("dom.popup_allowed_events", "");
 user_pref("dom.webnotifications.enabled", false);
 
 user_pref("extensions.getAddons.cache.enabled", false);
-user_pref("extensions.getAddons.showPane", false);
+user_pref("extensions.getAddons.showPane", false);                         // Disable recommendations pane
+user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);    // Disable addons recommendations
 user_pref("extensions.pocket.enabled", false);
 user_pref("extensions.screenshots.upload-disabled", true);
 user_pref("extensions.shield-recipe-client.api_url", "");
@@ -124,7 +134,7 @@ user_pref("experiments.activeExperiment", false);
 user_pref("geo.enabled", false);
 user_pref("gfx.webrender.all", true);
 user_pref("javascript.options.mem.max", 51200);
-user_pref("layout.css.visited_links_enabled", false);
+user_pref("layout.css.visited_links_enabled", false);                       // Disable visited links
 
 user_pref("media.autoplay.default", 1);
 user_pref("media.ffvpx.enabled", false);
@@ -133,7 +143,9 @@ user_pref("media.ffmpeg.vaapi.enabled", true);
 user_pref("media.ffmpeg.dmabuf-textures.enabled", true);
 
 user_pref("network.IDN_show_punycode", true);
+user_pref("network.allow-experiments", false);
 user_pref("network.captive-portal-service.enabled", false);
+user_pref("network.connectivity-service.enabled", false);                  // Disable network connectivity check
 user_pref("network.cookie.cookieBehavior", 1);
 user_pref("network.cookie.lifetimePolicy", 2);
 user_pref("network.cookie.thirdparty.sessionOnly", true);
@@ -141,15 +153,14 @@ user_pref("network.dns.disableIPv6", true);
 user_pref("network.dns.disablePrefetch", true);
 user_pref("network.dns.disablePrefetchFromHTTPS", true);
 user_pref("network.dnsCacheEntries", 0);
-user_pref("network.trr.mode", 5);
-user_pref("network.prefetch-next", false);
-user_pref("network.allow-experiments", false);
-user_pref("network.predictor.enabled", false);
-user_pref("network.predictor.cleaned-up", true);
-user_pref("network.proxy.socks_remote_dns", true);
 user_pref("network.http.referer.spoofSource", true);
-user_pref("network.predictor.enable-prefetch", false);
 user_pref("network.http.speculative-parallel-limit", 0);
+user_pref("network.predictor.cleaned-up", true);
+user_pref("network.predictor.enable-prefetch", false);
+user_pref("network.predictor.enabled", false);
+user_pref("network.prefetch-next", false);
+user_pref("network.proxy.socks_remote_dns", true);                          // Use proxy for DNS
+user_pref("network.trr.mode", 5);
 
 user_pref("security.OCSP.enabled", 0);
 user_pref("security.ssl.disable_session_identifiers", true);
@@ -172,17 +183,24 @@ user_pref("toolkit.telemetry.updatePing.enabled", false);
 
 user_pref("signon.autofillForms", false);
 user_pref("signon.generation.enabled", false);
+user_pref("signon.firefoxRelay.feature", "disabled");                      // Disable relay email masks
 
 user_pref("extensions.formautofill.addresses.enabled", false);
 user_pref("extensions.formautofill.creditCards.enabled", false);
 
-user_pref("privacy.donottrackheader.value", 1);
 user_pref("privacy.donottrackheader.enabled", true);
-user_pref("privacy.trackingprotection.enabled", true);
-user_pref("privacy.trackingprotection.pbmode.enabled", true);
+user_pref("privacy.donottrackheader.value", 1);
 user_pref("privacy.trackingprotection.cryptomining.enabled", true);
+user_pref("privacy.trackingprotection.enabled", true);
 user_pref("privacy.trackingprotection.fingerprinting.enabled", true);
+user_pref("privacy.trackingprotection.pbmode.enabled", true);
 user_pref("privacy.usercontext.about_newtab_segregation.enabled", true);
+
+user_pref("privacy.clearOnShutdown.cache", true);                           // Clear cache on shutdown
+user_pref("privacy.clearOnShutdown.cookies", true);                         // Clear cookies on shutdown
+user_pref("privacy.clearOnShutdown.formdata", true);                        // Clear form data on shutdown
+user_pref("privacy.clearOnShutdown.offlineApps", true);                     // Clear offline website data on shutdown
+user_pref("privacy.clearOnShutdown.sessions", true);                        // Clear active logins on shutdown
 
 user_pref("webgl.disable-extensions", true);
 user_pref("webgl.disabled", true);
