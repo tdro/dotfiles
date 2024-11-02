97c26f8281edf3cc9f9c8c7a664a38d60544c67f
tdro <tdro@users.noreply.github.com>
2024-11-02 00:00:00 +0000

    .config/psysh: Add config

---
+++ .config/psysh/config.php
@@ -0,0 +1,11 @@
+<?php
+
+// https://github.com/bobthecow/psysh/wiki/Sample-config
+
+return [
+    'pager' => 'cat',
+    'updateCheck' => 'never',
+    'theme' => [
+        'compact' => true,
+    ],
+];
