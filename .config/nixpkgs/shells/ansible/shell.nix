8448efae829d73d2436d2d59caed378d07e4a6f5
tdro <tdro@users.noreply.github.com>
2025-03-15 00:00:00 +0000

    .config/nixpkgs/shells/ansible: Update

--- .config/nixpkgs/shells/ansible/shell.nix
+++ .config/nixpkgs/shells/ansible/shell.nix
@@ -5,29 +5,31 @@ let
   name = "nix-shell.ansible";
 
   pkgs = import (builtins.fetchTarball {
-    url = "https://releases.nixos.org/nixos/21.11/nixos-21.11.336020.2128d0aa28e/nixexprs.tar.xz";
-    sha256 = "0w8plbxms0di6gnh0k2yhj0pgxzxas7g5x0m01zjzixf16i2bapj";
-  }) { };
+    url = "https://releases.nixos.org/nixos/24.11/nixos-24.11.710905.a0f3e10d9435/nixexprs.tar.xz";
+    sha256 = "01j4fa581yj276fyzsplizr82gckzwn3j6qlwk5phz4idz689v0y"; }) { };
 
   project = "${builtins.getEnv "HOME"}/Shares/Projects/infrastructure/ansible";
 
-  python = pkgs.python39.withPackages (ps: with ps; [ mitogen ]);
+  python = pkgs.python3.withPackages (ps: with ps; [ mitogen ]);
 
   shell = pkgs.writeShellApplication {
     inherit name;
     text = ''
       /usr/bin/env --ignore-environment /bin/sh -c ${
         pkgs.writeScript name ''
-          export HOME=/tmp
+          export HOME=/tmp/ansible
           export PS1='\h (${name}) \W \$ '
-          export PATH=${pkgs.lib.strings.makeBinPath [
-            python
-            pkgs.busybox
-            pkgs.openssh
-            pkgs.ansible_2_10
-          ]}
-          export ANSIBLE_STRATEGY_PLUGINS=${python}/lib/*/site-packages/ansible_mitogen/plugins
           export ANSIBLE_STRATEGY=mitogen_linear
+          export ANSIBLE_STRATEGY_PLUGINS=${python}/lib/*/site-packages/ansible_mitogen/plugins
+          export PATH=${
+            pkgs.lib.strings.makeBinPath [
+              python
+              pkgs.ansible
+              pkgs.busybox
+              pkgs.openssh
+            ]
+          }
+          mkdir --parents /tmp/ansible
           cd '${project}' || exit 1
           ssh -T git@github.com
           /bin/sh
