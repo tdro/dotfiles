eab5770586faefedb8b3fa1b7046aa327f2cca88
tdro <tdro@users.noreply.github.com>
2025-05-25 00:00:00 +0000

    .config/nvim: Add LSP and more mappings

--- .config/nvim/colors/druid.lua
+++ .config/nvim/colors/druid.lua
@@ -7,6 +7,7 @@ local colors = {
   white       = {fg = "#ffffff", bg = "#080808", ctermfg = 231, ctermbg = 232},
   red         = {fg = "#080808", bg = "#ff005f", ctermfg = 232, ctermbg = 197},
   teal        = {fg = "#00ffff", bg = "",        ctermfg = 14,  ctermbg = 0},
+  lightred    = {fg = "#080808", bg = "#ff8787", ctermfg = 0,   ctermbg = 210},
 
 }
 
@@ -16,10 +17,11 @@ local theme = {
   IncSearch = {fg = colors.black.fg, bg = colors.lightyellow.bg, ctermfg = colors.black.ctermfg, ctermbg = colors.lightyellow.ctermbg, cterm = { bold = true }},
   Search    = {fg = colors.black.fg, bg = colors.lightyellow.bg, ctermfg = colors.black.ctermfg, ctermbg = colors.lightyellow.ctermbg, cterm = { bold = true }},
 
-  StatusLine   = {fg = colors.black.fg, bg = colors.black.bg, ctermfg = colors.black.ctermfg, ctermbg = colors.black.ctermbg, cterm = { bold = false }},
-  StatusLineNC = {fg = colors.black.fg, bg = colors.black.bg, ctermfg = colors.black.ctermfg, ctermbg = colors.black.ctermbg, cterm = { bold = true  }},
-  ErrorMsg     = {fg = colors.red.fg,   bg = colors.red.bg,   ctermfg = colors.red.ctermfg,   ctermbg = colors.red.ctermbg,   cterm = { bold = true  }},
-  Visual       = {fg = colors.black.fg, bg = colors.black.bg, ctermfg = colors.black.ctermfg, ctermbg = colors.black.ctermbg, cterm = { bold = true  }},
+  StatusLine   = {fg = colors.black.fg,    bg = colors.black.bg,    ctermfg = colors.black.ctermfg,    ctermbg = colors.black.ctermbg,    cterm = { bold = false }},
+  StatusLineNC = {fg = colors.black.fg,    bg = colors.black.bg,    ctermfg = colors.black.ctermfg,    ctermbg = colors.black.ctermbg,    cterm = { bold = true  }},
+  ErrorMsg     = {fg = colors.red.fg,      bg = colors.red.bg,      ctermfg = colors.red.ctermfg,      ctermbg = colors.red.ctermbg,      cterm = { bold = true  }},
+  Visual       = {fg = colors.black.fg,    bg = colors.black.bg,    ctermfg = colors.black.ctermfg,    ctermbg = colors.black.ctermbg,    cterm = { bold = true  }},
+  qfError      = {fg = colors.lightred.bg, bg = colors.lightred.fg, ctermfg = colors.lightred.ctermbg, ctermbg = colors.lightred.ctermfg, cterm = { bold = true  }},
 
   String     = {fg = colors.grey.fg,  bg = "", ctermfg = colors.grey.ctermfg,  ctermbg = ""},
   Comment    = {fg = colors.black.bg, bg = "", ctermfg = colors.black.ctermbg, ctermbg = ""},
@@ -32,6 +34,9 @@ local theme = {
   Special           = { link = "Function"},
   NvimInternalError = { link = "ErrorMsg"},
   WarningMsg        = { link = "Search"},
+
+  DiagnosticUnderlineError = {fg = colors.lightred.bg,    bg = "", ctermfg = colors.lightred.ctermbg,    ctermbg = "", cterm = { bold = true, underline = false }},
+  DiagnosticUnderlineWarn  = {fg = colors.lightyellow.bg, bg = "", ctermfg = colors.lightyellow.ctermbg, ctermbg = "", cterm = { bold = true, underline = false }},
 
 }
 
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -24,6 +24,9 @@ vim.opt.completeopt    = ""         -- Disable <C-p> completion popup
 vim.opt.wildmenu       = false      -- Disable short command completion popup
 vim.opt.scrolloff      = 5          -- Set vertical scroll headroom
 vim.opt.sidescrolloff  = 10         -- Set horizontal scroll headroom
+vim.opt.signcolumn     = "no"       -- Disable diagnostics column left
+
+vim.opt.grepprg = "rg --no-heading --line-number --column --smart-case --multiline --regexp" -- Set search program
 
 vim.opt.shortmess:append("I")       -- Disable startup message
 vim.opt.fillchars:append("eob: ")   -- Disable end of buffer ~ character indicator
@@ -37,13 +40,15 @@ vim.opt.listchars = {               -- Set hidden character identifiers when `:s
 vim.keymap.set('n', '<leader>ev', ':tab drop ~/.config/nvim/init.lua<cr>')  -- Edit configuration
 vim.keymap.set('n', '<leader>rs', ':%s/\\s\\+$')                            -- Remove trailing whitespace
 vim.keymap.set('n', '<leader>grep', ':silent grep ')                        -- Run search functions
+vim.keymap.set('v', '<leader>grep', 'y/<C-R>*<cr>:silent grep <C-R>*<cr>')  -- Run search functions for symbol under cursor
+
 vim.keymap.set('n', '<Esc>', 'v<Esc>:nohl<cr>', { silent = true })          -- Exit incremental search
 
-vim.keymap.set('n', '<Tab><Tab>', ':redir @a | silent ls   | redir END | 10split [buffers] | %d | set ft=vim | put a | setlocal readonly | call feedkeys("")<cr>')
-vim.keymap.set('n', '<Tab>s',     ':redir @a | silent echo | redir END | 10split [scratch]      | set ft=vim | put a | setlocal readonly | call feedkeys("")<cr>')
-vim.keymap.set('n', '<Tab>t',     ':redir @a | silent tabs | redir END | 10split [tabs]    | %d | set ft=vim | put a | setlocal readonly | call feedkeys("gg3dd")<cr>')
-vim.keymap.set('n', '<Tab>m',     ':redir @a | silent map  | redir END | 10split [maps]    | %d | set ft=vim | put a | setlocal readonly | call feedkeys("gg3dd")<cr>')
-vim.keymap.set('n', '<Tab>c',     ':redir @a | silent hi   | redir END | 10split [colors]  | %d | set ft=vim | put a | setlocal readonly | call feedkeys("gg2dd")<cr>')
+vim.keymap.set('n', '<Tab><Tab>', ':redir @a | silent ls   | redir END | 10split [buffers] | %d | set ft=vim | put a | setlocal buftype=nofile | call feedkeys("")<cr>')
+vim.keymap.set('n', '<Tab>s',     ':redir @a | silent echo | redir END | 10split [scratch]      | set ft=vim | put a | setlocal buftype=nofile | call feedkeys("")<cr>')
+vim.keymap.set('n', '<Tab>t',     ':redir @a | silent tabs | redir END | 10split [tabs]    | %d | set ft=vim | put a | setlocal buftype=nofile | call feedkeys("gg3dd")<cr>')
+vim.keymap.set('n', '<Tab>m',     ':redir @a | silent map  | redir END | 10split [maps]    | %d | set ft=vim | put a | setlocal buftype=nofile | call feedkeys("gg3dd")<cr>')
+vim.keymap.set('n', '<Tab>c',     ':redir @a | silent hi   | redir END | 10split [colors]  | %d | set ft=vim | put a | setlocal buftype=nofile | call feedkeys("gg2dd")<cr>')
 
 vim.keymap.set('v', '<C-j>', ":m'>+<cr>gv", { silent = true })   -- Move visual selection down
 vim.keymap.set('v', '<C-k>', ":m -2<cr>gv", { silent = true })   -- Move visual selection up
@@ -68,26 +73,44 @@ vim.keymap.set('n', 'j', "j:call setreg('c', col('.'))<cr>", { silent = true }) 
 vim.keymap.set('n', 'k', "k:call setreg('c', col('.'))<cr>", { silent = true })  -- Persist cursor column
 vim.keymap.set('n', 'l', "l:call setreg('c', col('.'))<cr>", { silent = true })  -- Persist cursor column
 
+vim.keymap.set('n', '<Bslash>',   ":vsplit<cr>",             { desc = "Vertical split"               })
+vim.keymap.set('n', '<C-Bslash>', ":split<cr>",              { desc = "Horizontal split"             })
+vim.keymap.set('n', '<C-j>',      ":resize +5<cr>",          { desc = "Horizontal split resize down" })
+vim.keymap.set('n', '<C-k>',      ":resize -5<cr>",          { desc = "Horizontal split resize up"   })
+vim.keymap.set('n', '<C-h>',      ":vertical resize +5<cr>", { desc = "Vertical split resize left"   })
+vim.keymap.set('n', '<C-l>',      ":vertical resize -5<cr>", { desc = "Vertical split resize right"  })
+
+vim.keymap.set('n',       'grn', 'viwy/<C-R>*<cr>N:lua vim.lsp.buf.rename()<cr>',               { desc = 'Renames symbol under cursor'             })
+vim.keymap.set('n',       'gtd', 'viwy/<C-R>*<cr>N:lua vim.lsp.buf.definition()<cr>',           { desc = 'Jumps to definition under cursor'        })
+vim.keymap.set('n',       'grr', 'viwy/<C-R>*<cr>N:lua vim.lsp.buf.references()<cr>',           { desc = 'References to symbol under cursor'       })
+vim.keymap.set('n',       'gri', 'viwy/<C-R>*<cr>N:lua vim.lsp.buf.implementation()<cr>',       { desc = 'Implementations for symbol under cursor' })
+vim.keymap.set('n',       'gro', 'viwy/<C-R>*<cr>N:lua vim.lsp.buf.document_symbol()<cr>',      { desc = 'List symbols in current buffer'          })
+vim.keymap.set('n',       'gtt', 'viwy/<C-R>*<cr>N:lua vim.lsp.buf.type_definition()<cr>',      { desc = 'Jumps to type definition under cursor'   })
+vim.keymap.set('n',       'gdd', ':lua vim.diagnostic.setloclist()<cr>:lopen<cr>',              { desc = 'Open diagnostics in a location list'     })
+vim.keymap.set({'n','x'}, 'gra', function() vim.lsp.buf.code_action() end,                      { desc = 'vim.lsp.buf.code_action()'               })
+
 vim.keymap.set('v', '<leader>sn', "<Esc>:call setreg('c', col('.'))<cr>:call setreg('l', line('.'))<cr>gv!perl -e 'print sort { length($a) <=> length($b) } <>'<cr>:call cursor(getreg('l'), getreg('c'))<cr>", { silent = true, desc = 'Sort lines by length' })
 
 -- Auto commands
-autocommands = vim.api.nvim_create_augroup('', { clear = true })
+vim.api.nvim_create_augroup('autocommands', { clear = true })
 
 -- Source reloads
-vim.api.nvim_create_autocmd({"BufWritePost"}, { group = autocommands, pattern = {"init.lua"}, callback = function()
+vim.api.nvim_create_autocmd({"BufWritePost"}, { group = 'autocommands', pattern = {"init.lua"}, callback = function()
   vim.cmd(':source %')                -- Auto reload configuration
   vim.bo.filetype = vim.bo.filetype   -- Retrigger FileType events
 end, })
 
 --  Format using visual select + gq
-vim.api.nvim_create_autocmd({"FileType"}, { group = autocommands, pattern = "lua", callback = function() vim.opt_local.formatprg = "lua-format" end, })
+vim.api.nvim_create_autocmd({"FileType"}, { group = 'autocommands', pattern = "lua", callback = function() vim.opt_local.formatprg = "lua-format" end, })
 
 -- Keyword documentation lookups
-vim.api.nvim_create_autocmd({"FileType"}, { group = autocommands, pattern = {"lua"}, callback = function() vim.keymap.set('n', 'K', 'viwy/<C-R>*<cr>N:h <C-R>*<cr><C-w>w') end })
-vim.api.nvim_create_autocmd({"FileType"}, { group = autocommands, pattern = {"lua"}, callback = function() vim.keymap.set('v', 'K',    'y/<C-R>*<cr>N:h <C-R>*<cr><C-w>w') end })
+vim.api.nvim_create_autocmd({"FileType"}, { group = 'autocommands', pattern = {"lua"},  callback = function() vim.keymap.set('n', 'K', 'viwy/<C-R>*<cr>N:h <C-R>*<cr><C-w>w', { buffer = true }) end })
+vim.api.nvim_create_autocmd({"FileType"}, { group = 'autocommands', pattern = {"lua"},  callback = function() vim.keymap.set('v', 'K',    'y/<C-R>*<cr>N:h <C-R>*<cr><C-w>w', { buffer = true }) end })
+vim.api.nvim_create_autocmd({"FileType"}, { group = 'autocommands', pattern = {"help"}, callback = function() vim.keymap.set('n', 'K', 'viwy/<C-R>*<cr>N:h <C-R>*<cr>',       { buffer = true }) end })
+vim.api.nvim_create_autocmd({"FileType"}, { group = 'autocommands', pattern = {"help"}, callback = function() vim.keymap.set('v', 'K',    'y/<C-R>*<cr>N:h <C-R>*<cr>',       { buffer = true }) end })
 
 -- REPL Commands
-vim.api.nvim_create_autocmd({"FileType"}, { group = autocommands, pattern = {"lua"},
+vim.api.nvim_create_autocmd({"FileType"}, { group = 'autocommands', pattern = {"lua"},
 callback = function()
   vim.keymap.set('n', '<leader>cc', 'Vy:lua <C-R>*<cr>', { buffer = true })
   vim.keymap.set('v', '<leader>cc',  'y:lua <C-R>*<cr>', { buffer = true })
@@ -96,25 +119,39 @@ callback = function()
 end
 })
 
+vim.api.nvim_create_autocmd({"FileType"}, { group = 'autocommands', pattern = {"vim"},
+callback = function()
+  vim.keymap.set('n', '<leader>cc', 'yVy:<C-R>*<cr>',   { buffer = true })
+  vim.keymap.set('n', '<leader>co', 'yVy:<C-R>*<cr>',   { buffer = true })
+  vim.keymap.set('v', '<leader>co',   "y:<C-w>redir @a | silent! exe substitute('<C-R>*', '\\r', '', 'g') | redir END | call feedkeys('gvy$\"ap')<cr>", { buffer = true })
+end
+})
+
 -- Quick fix commands
 vim.api.nvim_create_autocmd({"FileType"}, {
-    group = autocommands,
+    group = 'autocommands',
     pattern = {"qf"},
     callback = function()
       vim.keymap.set('n', '<cr>', "<cr><C-w>w", { silent = true, buffer = true })
-      vim.keymap.set('n', 'j',    "j:cnext<cr><C-w>w:call cursor(0, getreg('c'))<cr>", { silent = true, buffer = true })
-      vim.keymap.set('n', 'k',    "k:cprev<cr><C-w>w:call cursor(0, getreg('c'))<cr>", { silent = true, buffer = true })
-      switch = vim.api.nvim_create_augroup('', { clear = true })
-      vim.api.nvim_create_autocmd({"BufRead"}, { group = switch, pattern = {"*"},
+      vim.keymap.set('n', 'j',    "j:silent!cnext|silent!lnext<cr><C-w>w:call cursor(0, getreg('c'))<cr>", { silent = true, buffer = true })
+      vim.keymap.set('n', 'k',    "k:silent!cprev|silent!lprev<cr><C-w>w:call cursor(0, getreg('c'))<cr>", { silent = true, buffer = true })
+      vim.api.nvim_create_augroup('switch', { clear = true })
+      vim.api.nvim_create_autocmd({"BufRead"}, { group = 'switch', pattern = {"*"},
       callback = function()
-        vim.cmd('wincmd p | call feedkeys("gg")')
-        vim.api.nvim_clear_autocmds({ group = switch })
+        vim.cmd(':silent! wincmd p | call feedkeys("gg")')
+        vim.api.nvim_clear_autocmds({ group = 'switch' })
       end
       })
     end
 })
 
-vim.api.nvim_create_autocmd({"QuickFixCmdPost"}, { group = autocommands, pattern = {"*"}, command = ":copen" })
+vim.api.nvim_create_autocmd({"QuickFixCmdPost"}, { group = 'autocommands', pattern = {"*"}, command = ":cclose | bufdo bd! |:copen" })
 
 -- Auto save
-vim.api.nvim_create_autocmd({"InsertLeave", "CursorHold"}, { group = autocommands, pattern = {"*"}, command = ":silent! write | echo '[filetype=' . &filetype . ']'" })
+vim.api.nvim_create_autocmd({"InsertLeave", "CursorHold"}, { group = 'autocommands', pattern = {"*"}, command = ":silent! write | echo '[filetype=' . &filetype . ']'" })
+
+-- Language server protocol (LSP) configuration
+vim.lsp.config['clangd'] = { cmd = {'clangd'}, filetypes = {"c", "cpp", "objc", "objcpp", "cuda", "proto"} }
+
+-- Language server protocol (LSP) enable
+vim.lsp.enable({'clangd'})
