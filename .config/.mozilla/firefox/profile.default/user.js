c0b75e6ce4dd64eb037e8e8275b96e8a51e2da14
tdro <tdro@users.noreply.github.com>
2025-08-30 00:00:00 +0000

    .config/.mozilla/firefox/profile.default: Update

--- .config/.mozilla/firefox/profile.default/user.js
+++ .config/.mozilla/firefox/profile.default/user.js
@@ -140,6 +140,7 @@ user_pref("browser.urlbar.suggest.history", false);
 user_pref("browser.urlbar.suggest.openpage", false);
 user_pref("browser.urlbar.suggest.searches", false);
 user_pref("browser.urlbar.suggest.topsites", false);
+user_pref("browser.urlbar.suggest.trending", false);
 user_pref("browser.urlbar.suggest.recentsearches", false);
 user_pref("browser.urlbar.suggest.quicksuggest.sponsored", false);
 user_pref("browser.urlbar.suggest.quicksuggest.nonsponsored", false);
